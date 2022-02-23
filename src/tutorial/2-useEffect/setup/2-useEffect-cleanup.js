24cbf2d85be7346143dae94d7728a5178788f1e7
Ryotaro Kurita <ryo.kuri2525@gmail.com>
2022-02-23 22:38:51 +0900

    finish use effect

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,29 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  // int meanse order of expression after rendering
+  useEffect(() => {
+    //3
+    console.log('useEfect');
+    window.addEventListener('resize', checkSize);
+    return () => {
+      //2
+      console.log('clean up');
+      window.removeEventListener('resize', checkSize);
+    }
+  });
+  //1
+  console.log('render');
+  return <>
+   <h1>Window</h1>
+   <h2>{size}.px</h2>
+  </>;
 };
 
 export default UseEffectCleanup;
