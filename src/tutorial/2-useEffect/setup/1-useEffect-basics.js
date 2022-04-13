5b3ed0f116aa1e134b548a2e51d2ba96cd9c71d4
Ryotaro Kurita <ryo.kuri2525@gmail.com>
2022-04-13 09:45:15 +0900

    set up

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,26 +1,27 @@
 import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
-// cleanup function
 // second parameter
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
 
-  useEffect(() => {
-    console.log('call useEfect');
-    if(value >= 1) {
-      document.title = `New message(${value})`;
-    }
-  }, [value]);
+  //パターン1
+  console.log('レンダリングの度に呼び出し');
 
+  //パターン2
   useEffect(() => {
-    console.log('hello world');
+    console.log('初回レンダリング時のみ呼び出し');
   }, [])
 
-  console.log('render component');
+  //パターン3
+  useEffect(() => {
+    console.log('valueが更新される度に呼び出し');
+  }, [value]);
+
+  //state更新するためのボタン
   return (
   <>
     <h1>{value}</h1>
-    <button className='btn' onClick={() => setValue(value + 1)}>click</button>
+    <button className='btn' onClick={() => setValue(value + 1)}>クリック</button>
   </>
   );
 };
